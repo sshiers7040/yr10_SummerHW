2b3564b6ee037e98260c09e1ec041b633ab8a8ef
sshiers7040 <sshiers7040@sohamcollege.org.uk>
2014-07-03 21:25:31 +0100

    Update sprite.py
    Commited 3/7/14 - completed in June
    I have created my own sprite of a coin from an old Mario game.

--- HW/5/sprite.py
+++ HW/5/sprite.py
@@ -2,46 +2,40 @@ import pygame, sys #Import the pygame and sytem modules
 
 pygame.init() #Allow pygame to initiate properly.
 
-screen = pygame.display.set_mode([225, 150]) #The scrren object
+screen = pygame.display.set_mode([350, 400]) #The scrren object
 
-r = pygame.Color("red") #Set the variable r to pygame's red colour
+b = pygame.Color("black") #Set the variable r to pygame's black colour
 w = pygame.Color("white") #Set the variable w to pygame's white colour
+y = pygame.Color("yellow") #Set the variable y to pygame's yellow colour
 
-frameOne = [ #The colour grid for the first frame
-  [ w, w, r, r, r, r, r, w, w ],
-  [ w, w, r, w, r, w, r, w, w ],
-  [ w, w, r, r, r, r, r, w, w ],
-  [ w, r, r, r, w, r, r, r, w ],
-  [ r, w, w, r, r, r, w, w, r ],
-  [ r, r, w, w, w, w, w, r, r ]
+data = [
+  [ w, w, w, w, b, b, b, b, b, b, w, w, w, w ],
+  [ w, w, b, b, b, w, w, w, b, b, b, b, w, w ],
+  [ w, b, b, w, w, y, y, y, y, y, b, b, w, w ],
+  [ w, b, w, y, y, w, w, w, b, y, y, b, b, w ],
+  [ b, b, w, y, y, w, y, y, b, y, y, b, b, w ],
+  [ b, w, y, y, y, w, y, y, b, y, y, y, b, b ],
+  [ b, w, y, y, y, w, y, y, b, y, y, y, b, b ],
+  [ b, w, y, y, y, w, y, y, b, y, y, y, b, b ],
+  [ b, w, y, y, y, w, y, y, b, y, y, y, b, b ],
+  [ b, w, y, y, y, w, y, y, b, y, y, y, b, b ],
+  [ b, w, y, y, y, w, y, y, b, y, y, y, b, b ],
+  [ b, b, w, y, y, w, y, y, b, y, y, b, b, w ],
+  [ w, b, w, y, y, b, b, b, b, y, y, b, b, w ],
+  [ w, b, b, w, y, y, y, y, y, y, b, b, w, w ],
+  [ w, w, b, b, b, w, w, w, b, b, b, b, w, w ],
+  [ w, w, w, w, b, b, b, b, b, b, w, w, w, w ],
   ]
 
-frameTwo = [ #The colour grid for the second frame
-  [ w, w, w, w, w, w, w, w, w ],
-  [ w, w, r, r, r, r, r, w, w ],
-  [ w, w, r, w, r, w, r, w, w ],
-  [ r, w, r, r, r, r, r, w, r ],
-  [ r, r, r, r, w, r, r, r, r ],
-  [ w, w, w, r, r, r, w, w, w ]
-  ]
+for y, row in enumerate(data): #For each row in data
+    for x, colour in enumerate(row): #For each colour in the row
+        rect = pygame.Rect(x*25, y*25, 25, 25) #Create the rect of colour for that item in the row
+        screen.fill(colour, rect=rect) #Fill colour to the screen in the rect
 
-def drawFrame(screen, frame):
-    for y, row in enumerate(frame): #For each row in the frame
-        for x, colour in enumerate(row): #For each colour in the row
-            rect = pygame.Rect(x*25, y*25, 25, 25) #Create the rect of colour for that item in the row
-            screen.fill(colour, rect=rect) #Fill colour to the screen in the rect
+pygame.display.update() #Update the screen
 
 while True: #While the program is running
     for event in pygame.event.get(): #if any events are detected
         if event.type == pygame.QUIT: #If the event is to close the window
             pygame.quit() #Clear up pygame and stop it
             sys.exit() #Stop the system
-
-    drawFrame(screen, frameOne) #Draw the first frame
-    pygame.display.update() #Update the screen
-    pygame.time.wait(1000) #Wait a second
-    drawFrame(screen, frameTwo) #Draw the second frame
-    pygame.display.update() #Update the screen
-    pygame.time.wait(150) #Wait a 15% of a second
-
-
