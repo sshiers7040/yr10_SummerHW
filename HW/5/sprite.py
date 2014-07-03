a526adec86b235d3c84fff92d19e576b488ebf15
sshiers7040 <sshiers7040@sohamcollege.org.uk>
2014-07-03 21:24:13 +0100

    Update sprite.py
    Committed 3/7/14 - completed in June
    I have animated the space invader.

--- HW/5/sprite.py
+++ HW/5/sprite.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode([225, 150]) #The scrren object
 r = pygame.Color("red") #Set the variable r to pygame's red colour
 w = pygame.Color("white") #Set the variable w to pygame's white colour
 
-data = [
+frameOne = [ #The colour grid for the first frame
   [ w, w, r, r, r, r, r, w, w ],
   [ w, w, r, w, r, w, r, w, w ],
   [ w, w, r, r, r, r, r, w, w ],
@@ -16,15 +16,32 @@ data = [
   [ r, r, w, w, w, w, w, r, r ]
   ]
 
-for y, row in enumerate(data): #For each row in data
-    for x, colour in enumerate(row): #For each colour in the row
-        rect = pygame.Rect(x*25, y*25, 25, 25) #Create the rect of colour for that item in the row
-        screen.fill(colour, rect=rect) #Fill colour to the screen in the rect
+frameTwo = [ #The colour grid for the second frame
+  [ w, w, w, w, w, w, w, w, w ],
+  [ w, w, r, r, r, r, r, w, w ],
+  [ w, w, r, w, r, w, r, w, w ],
+  [ r, w, r, r, r, r, r, w, r ],
+  [ r, r, r, r, w, r, r, r, r ],
+  [ w, w, w, r, r, r, w, w, w ]
+  ]
 
-pygame.display.update() #Update the screen
+def drawFrame(screen, frame):
+    for y, row in enumerate(frame): #For each row in the frame
+        for x, colour in enumerate(row): #For each colour in the row
+            rect = pygame.Rect(x*25, y*25, 25, 25) #Create the rect of colour for that item in the row
+            screen.fill(colour, rect=rect) #Fill colour to the screen in the rect
 
 while True: #While the program is running
     for event in pygame.event.get(): #if any events are detected
         if event.type == pygame.QUIT: #If the event is to close the window
             pygame.quit() #Clear up pygame and stop it
             sys.exit() #Stop the system
+
+    drawFrame(screen, frameOne) #Draw the first frame
+    pygame.display.update() #Update the screen
+    pygame.time.wait(1000) #Wait a second
+    drawFrame(screen, frameTwo) #Draw the second frame
+    pygame.display.update() #Update the screen
+    pygame.time.wait(150) #Wait a 15% of a second
+
+
